0fbea308f0f3fff32c04db7f395d76ef410f93e6
MBP-2013 <mbp-2013@MBP-2013s-MacBook-Pro.local>
2025-12-06 07:46:30 +0700

    add some lab04 files

---
+++ lab04/ws441.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    printf("Calculation\n");
+    printf ("Enter Value of a: ");
+    scanf ("%d", &a);
+    printf ("Enter Value of b: ");
+    scanf ("%d", &b);
+    c = 77*(6/11)*a + (3/8)*b ;
+    printf ("Result value of z : %d", c);
+}
