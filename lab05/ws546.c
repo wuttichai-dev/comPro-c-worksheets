659ae330f695b57198e84e5252a5ed12ff02f598
wuttichai.vjk <wuttichai.vjk@gmail.com>
2025-12-08 22:10:20 +0700

    add ws546

---
+++ lab05/ws546.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    int upperint, lowerint;
+    printf ("Input Data\n");
+    printf ("Enter Integer Value1: ");
+    scanf ("%d",&upperint);
+    printf ("Enter Integer Value2: ");
+    scanf ("%d",&lowerint);
+    printf ("\nOutput Data\n");
+    printf ("Two Value of Integer are %d and %d\n",upperint , lowerint); 
+}
